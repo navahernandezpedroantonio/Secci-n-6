89ef6da327385be18c2f0d0be7bd8fe92c0c85a7
navahernandezpedroantonio <81387914+navahernandezpedroantonio@users.noreply.github.com>
2021-03-26 00:43:22 -0600

    Add files via upload

---
+++ Countdown.java
@@ -0,0 +1,16 @@
+//Pedro Antonio Nava Hernández Programación Avanzada 
+package com.example;
+
+public class Countdown {
+
+    public static void main(String[] args) {
+        
+        System.out.println("Countdown to Launch: ");
+
+        for(int i = 0; i <= 5; i++) {
+            System.out.print(i +" "); 
+        }
+
+        System.out.println("Blast On!");
+    }
+}
---
+++ SquareRootWhile.java
@@ -0,0 +1,21 @@
+//Pedro Antonio Nava Hernández Programación Avanzada 
+package com.example;
+
+import java.util.Scanner;
+
+
+public class SquareRootWhile {
+    public static void main(String args[])
+    {
+    System.out.print("Type a non-negative integer: ");
+     Scanner console = new Scanner(System.in);
+	int number = console.nextInt();
+while(number !=1){
+  System.out.println("Se realiza la raíz cuadrada ");
+  number++;
+}
+	
+    
+}
+    
+}
